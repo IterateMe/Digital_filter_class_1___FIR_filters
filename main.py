883511c55a76125a0572279c8a6db189a5c3f5fc
Vianney Gall <vianneygall@gmail.com>
2021-10-03 09:55:02 -0400

    small update

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from signal_data import Signal_data as SD
-
-SOURCE_FILE = "signals\\note_basson_plus_sinus_1000_Hz.wav"
+import numpy as np
+source_guit = "signals\\note_guitare_LAd.wav"
+source_basson = "signals\\note_basson_plus_sinus_1000_Hz.wav"
 
 
 if __name__ == '__main__':
-    print("Starting program . . .")
-    LaD = SD("LaD", SOURCE_FILE, (47545, 49823))
-    #LaD.show_freq_amp()
-    LaD.show_enveloppe_temp()
+    print("Starting program . . .\n")
+    #guit = SD("LaD a la Guitare", source_guit, (8481, 8860), 5)
+    #guit.show_freq_amp()
+    #guit.show_enveloppe_temp()
+    basson = SD("Basson", source_basson, True)
 
--- signal_data.py
+++ signal_data.py
@@ -6,10 +6,8 @@ import visualise as vis
 import FIR
 
 class Signal_data():
-    def __init__(self, name, source_file, fft_window):
-        print("Generating all the data for {} . . .".format(name))
-
-        self.fft_win = fft_window
+    def __init__(self, name, source_file, inspect):
+        print("Generating basic data for {} . . .".format(name))
         # Definir le nom du signal
         self.name = name
 
@@ -17,47 +15,80 @@ class Signal_data():
         self.wav = wf.read(source_file)
         self.datarate = self.wav[0]
         self.time_y = np.array(self.wav[1], dtype=float)
+        self.time_x = np.array([i for i in range(len(self.time_y))])  # Index des valeurs du fichier
 
-        #
-        self.x = np.array([i for i in range(len(self.time_y))]) # Index des valeurs du fichier
-        self.freq = np.fft.fft(self.time_y[fft_window[0]:fft_window[1]])
-        self.freqDb = np.log10(self.freq)*20
-        self.freq_norm = np.array([2*math.pi*i/len(self.time_y) for i in range(len(self.time_y))])
+        # Creer les array necessaires au traitement de signal
+        self.freq = None
+        self.freqDb = None
+        self.freq_norm = np.array([2*math.pi*i/self.datarate for i in range(self.datarate)])
+        self.main_sin = None
+
+        if inspect is True:
+            self.generate_fft(0,len(self.time_x))
+            self.show_freq_amp()
+
+
+    def generate_fft(self, start, end):
+        print ("Generating fft for {} . . .".format(self.name))
+        self.freq = np.fft.fft(self.time_y[start:end])
+        self.freqDb = np.log10(np.abs(self.freq)) * 20
+
+    def extract_main_sin(self, amp_diff):
+        print("Extracting main sin for {} . . .".format(self.name))
+        if self.freq == None:
+            print("fft must be defined in order to extract main sin")
+        N = (self.fft_win[1] - self.fft_win[0])
+        ref_freq = self.time_x[0:N // 2] * self.datarate / N
+        main_sin_index = []
+        main_sin_freq = []
+        main_sin_amp = []
+        for i in range(1, len(self.freqDb)//2 - 1):
+            if (self.freqDb[i] > self.freqDb[i-1] + amp_diff) and ( self.freqDb[i] > self.freqDb[i+1] + amp_diff ):
+                main_sin_index.append(i)
+                main_sin_freq.append(ref_freq[i])
+                main_sin_amp.append(self.freqDb[i])
+        print("\n{} sin have been extracted for {}:".format(len(main_sin_amp), self.name))
+        print("\t\tFREQ\t\t:\t\tAMP")
+        for i in range(len(main_sin_freq)):
+            print("\t{} : {}".format(main_sin_freq[i], main_sin_amp[i]))
+
+        self.main_sin = (main_sin_index, main_sin_amp)
 
 
     def show_freq_amp(self):
         title = "Transformée de fourier du signal {}".format(self.name)
-        N = (self.fft_win[1] - self.fft_win[0])
+        N = len(self.time_x)
         vis.show(title,
-                 self.x, "Temps (s)",
+                 self.time_x, "Temps (s)",
                  self.time_y, "Amplitude",
-                 self.x[0:N//2]*self.datarate/N, "Freq (Hz)",
+                 self.time_x[0:N // 2] * self.datarate / N, "Freq (Hz)",
                  np.abs(self.freqDb[0:N//2]), "Amplitude") # N'affiche que la première moitiée car la deuxième n'est qu'un reflet de la première
 
     def show_freq_angle(self):
         title = "Déphasage du signal {}".format(self.name)
         vis.show(title,
-                 self.x, "Temps (s)",
+                 self.time_x, "Temps (s)",
                  self.time_y, "Amplitude",
-                 self.x, "m",
+                 self.time_x, "m",
                  np.angle(self.freq), "Déphasage (rad)")
 
     def show_freq_normalized(self):
         vis.show("Valeurs de la fréquence normalisée de {} en fonction de son index".format(self.name),
-                 self.x, "Index",
+                 self.time_x, "Index",
                  self.freq_norm, "Valeur (rad)")
 
     def show_enveloppe_temp(self):
         freq_redressed = abs(self.time_y)
-        filtre = FIR.passe_bas(20000, 10)
-
-        vis.show("Enveloppe du signal {}".format(self.name),
-                 np.array([i for i in range(len(filtre))]), "Temps (s)",
-                 filtre, "Amplitude")
+        filtre = FIR.passe_bas(self.datarate, 22)
+        # filtre = np.log10(filtre) * 20
+        #
+        # vis.show("Filtre pour enveloppe du {}".format(self.name),
+        #          np.array([i for i in range(len(filtre))]), "Temps (s)",
+        #          filtre, "Amplitude")
 
         enveloppe = np.convolve( filtre, freq_redressed)
         vis.show("Enveloppe du signal {}".format(self.name),
                  np.array([i for i in range(len(enveloppe))]), "Temps (s)",
                  enveloppe, "Amplitude",
-                 self.x, "Temps (s)",
+                 self.time_x, "Temps (s)",
                  self.time_y, "Amplitude")
